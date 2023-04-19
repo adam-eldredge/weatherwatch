9e40fd0c5d57b0c0eb85c544be7409c0364fc842
adameldredge <43678759+adameldredge@users.noreply.github.com>
2023-04-19 02:09:51 -0400

    redesigned create, favorites, examples, added button for viewing number of tuples.

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,18 +1,52 @@
 import React from "react";
 import '../styles.css';
-//import App from "../App";
-// ^This will be used to link to the sign up page
+import 'react-widgets/styles.css';
+import Combobox from 'react-widgets/Combobox';
+import { useState } from "react";
 
 function Favorites() {
+
+    const[query,setQuery] = useState(null);
+
+    const renderItem = ({ item }) => (
+        <>
+            <span
+                style = {{
+                    height: 18,
+                    display: "inline-block",
+                    verticalAlign: "text-bottom",
+                    backgroundColor: item.toLowerCase(),
+                }}
+            />
+            { item }
+        </>
+    );
+
     return (
         <div className = 'page-container'>
             <div className="page-text">
             <h1>Favorites</h1>
-                
-            <div className = 'loginblock'>
-                Insert favorites here
-            </div>
-
+                <div className = 'block' style={{justifyContent:'center'}}>
+                    Select a Favorited Query:
+                    <Combobox
+                        style={{width: '50%', marginTop:'2%',  marginLeft: 'auto', marginRight: 'auto'}}
+                            defaultValue="Select Query"
+                            renderListItem={renderItem}
+                            query = { query }
+                            onChange={query => setQuery(query)}
+                            data={['Favorite Query 1', 'Favorite Query 2', 'Favorite Query 3', 
+                                    'Favorite Query 4', 'Favorite Query 5']}
+                    />
+                    <button className="button" style={{marginTop:'5%'}}>
+                            Generate
+                            {
+                                // This is where you would say onSubmit, take all values
+                                // If missing necessary values, reject and output error
+                                // Else, generate sql query and send to our weather database
+                                // With the tuples that get passed back, figure out how to output data.
+                            }
+                    </button>
+                </div>
             </div>
         </div>
     )
