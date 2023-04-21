58def2994d11d46392dc86f05a6197e6ef360033
Piyumath <68487811+piyumathh@users.noreply.github.com>
2023-04-21 02:23:50 -0400

    examples implementation
    outputs different result as data on each example selected. only 1 and 2 implemented so far. adding a query to the commented lines on server.js will make the others work too. (data all clustered together, didnt bother organzing since we're gonna put it in a graph anyway, but u can inspect element and go to console to see a formatted view of the data)

--- src/pages/Examples.jsx
+++ src/pages/Examples.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import '../styles.css';
+import axios from "axios";
 import 'react-widgets/styles.css';
 import Combobox from 'react-widgets/Combobox';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Examples() {
 
@@ -22,6 +23,23 @@ function Examples() {
         </>
     );
 
+    const [message, setMessage] = useState("");
+    const getInfo = (url) => {
+        try{
+            axios.get(url)
+            .then((res)=>{
+            console.log(res.status)
+            console.log(res.data)
+            setMessage(res.data)
+            }) 
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    //useEffect(() => {getInfo()}, []);
+
+
     return (
         <div className = 'page-container'>
             <div className="page-text">
@@ -33,13 +51,28 @@ function Examples() {
                             defaultValue="Select Query"
                             renderListItem={renderItem}
                             query = { query }
-                            onChange={query => setQuery(query)}
+                            onChange={query => {
+                                setQuery(query);
+                                setMessage('');     //this resets the message every change
+                            }}
                             data={['Example Query 1', 'Example Query 2', 'Example Query 3', 
                                     'Example Query 4', 'Example Query 5']}
                     />
-                    <button className="button" style={{marginTop:'5%'}}>
+                    <button className="button" style={{marginTop:'5%'}} onClick={()=>{
+                        if (query === 'Example Query 1')
+                            getInfo('/examples1');
+                        else if (query === 'Example Query 2')
+                            getInfo('/examples2');
+                        else if (query === 'Example Query 3')
+                            getInfo('/examples3');
+                        else if (query === 'Example Query 4')
+                            getInfo('/examples4');
+                        else if (query === 'Example Query 5')
+                            getInfo('/examples5');
+                    }}>
                             Generate
                             {
+                                <div>{message}</div>
                                 // This is where you would say onSubmit, take all values
                                 // If missing necessary values, reject and output error
                                 // Else, generate sql query and send to our weather database
