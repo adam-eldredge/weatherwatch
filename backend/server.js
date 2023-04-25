978ad64ecfbcdb105976a6641947face139e0d6a
Piyumath <68487811+piyumathh@users.noreply.github.com>
2023-04-25 04:14:36 -0400

    example queries and such

--- backend/server.js
+++ backend/server.js
@@ -157,11 +157,14 @@ app.post('/examples2', async (req, res) => {
 
 app.post('/examples3', async (req, res) => {
     const cityName = req.body.cityName.cityName;
-    console.log("City Name: " + cityName);
+    const city2Name = req.body.city2Name.city2Name;
+    console.log("City Name: " + cityName + "City 2 Name: " + city2Name);
     try{
         const connection = await oracledb.getConnection(config);
-        const ex3query = "SELECT extract(month from entrydate) AS x, MAX(CASE WHEN city = 'Tunis' THEN tempmax ELSE NULL END) AS tunis_y,MAX(CASE WHEN city = 'Cairo' THEN tempmax ELSE NULL END) AS cairo_y FROM adameldredge.weather WHERE city IN ('Tunis', 'Cairo') GROUP BY extract(month from entrydate) ORDER BY x";   
-        const result = await connection.execute(ex3query);
+        //const ex3query = "SELECT extract(month from entrydate) AS x, MAX(CASE WHEN city = 'Tunis' THEN tempmax ELSE NULL END) AS tunis_y,MAX(CASE WHEN city = 'Cairo' THEN tempmax ELSE NULL END) AS cairo_y FROM adameldredge.weather WHERE city IN ('Tunis', 'Cairo') GROUP BY extract(month from entrydate) ORDER BY x";   
+        const result = await connection.execute("SELECT extract(month from entrydate) AS x, MAX(CASE WHEN city = :cityName THEN tempmax ELSE NULL END) AS tunis_y,MAX(CASE WHEN city = :city2Name THEN tempmax ELSE NULL END) AS cairo_y FROM adameldredge.weather GROUP BY extract(month from entrydate) ORDER BY x",
+        [cityName, city2Name]);
+        console.log("City Name: " + cityName + " City 2 Name: " + city2Name)
         console.log(result.rows)
         res.json(result.rows);
         connection.close();
@@ -174,11 +177,12 @@ app.post('/examples3', async (req, res) => {
 
 app.post('/examples4', async (req, res) => {
     const cityName = req.body.cityName.cityName;
-    console.log("City Name: " + cityName);
+    const city2Name = req.body.city2Name.city2Name;
+    console.log("City Name: " + cityName + " City 2 Name: " + city2Name);
     try{
         const connection = await oracledb.getConnection(config);
-        const ex4query = "";    //this should be a query (sql statement in a string)
-        const result = await connection.execute(ex4query);
+        const result = await connection.execute("SELECT EXTRACT(MONTH FROM a.ENTRYDATE) AS Month, AVG(a.WINDSPEED) AS city1wind, b.avg_windspeed AS city2wind FROM ADAMELDREDGE.WEATHER a JOIN (SELECT EXTRACT(MONTH FROM ENTRYDATE) AS month, AVG(Windspeed) AS avg_windspeed FROM ADAMELDREDGE.WEATHER WHERE City = :cityName AND WINDSPEED IS NOT NULL AND EXTRACT(YEAR FROM ENTRYDATE) = '2021' GROUP BY EXTRACT(MONTH FROM ENTRYDATE) ) b ON EXTRACT(MONTH FROM a.ENTRYDATE) = b.month WHERE a.WINDSPEED IS NOT NULL AND a.City = :city2Name AND EXTRACT(YEAR FROM a.ENTRYDATE) = '2021' GROUP BY EXTRACT(MONTH FROM a.ENTRYDATE), b.avg_windspeed ORDER BY 1", 
+        [cityName, city2Name]);
         console.log(result.rows)
         res.json(result.rows);
         connection.close();
@@ -191,11 +195,32 @@ app.post('/examples4', async (req, res) => {
 
 app.post('/examples5', async (req, res) => {
     const cityName = req.body.cityName.cityName;
-    console.log("City Name: " + cityName);
+    const city2Name = req.body.city2Name.city2Name;
+    console.log("City Name: " + cityName + " City Name: " + cityName);
     try{
         const connection = await oracledb.getConnection(config);
         const ex5query = "";    //this should be a query (sql statement in a string)
-        const result = await connection.execute(ex5query);
+        const result = await connection.execute("SELECT CASE WHEN EXTRACT(MONTH FROM a.ENTRYDATE) IN (12, 1, 2) THEN 'Winter' WHEN EXTRACT(MONTH FROM a.ENTRYDATE) IN (3, 4, 5) THEN 'Spring' WHEN EXTRACT(MONTH FROM a.ENTRYDATE) IN (6, 7, 8) THEN 'Summer' ELSE 'Fall' END AS Season, AVG(a.WINDSPEED) AS City1, b.avg_windspeed AS City2 FROM ADAMELDREDGE.WEATHER a JOIN ( SELECT EXTRACT(MONTH FROM ENTRYDATE) AS month, AVG(Windspeed) AS avg_windspeed FROM ADAMELDREDGE.WEATHER WHERE City = :cityName AND WINDSPEED IS NOT NULL AND EXTRACT(YEAR FROM ENTRYDATE) BETWEEN '2019' AND '2021' GROUP BY EXTRACT(MONTH FROM ENTRYDATE) ) b ON EXTRACT(MONTH FROM a.ENTRYDATE) = b.month WHERE a.WINDSPEED IS NOT NULL AND a.City = :city2Name AND EXTRACT(YEAR FROM a.ENTRYDATE) BETWEEN '2019' AND '2021' GROUP BY CASE WHEN EXTRACT(MONTH FROM a.ENTRYDATE) IN (12, 1, 2) THEN 'Winter' WHEN EXTRACT(MONTH FROM a.ENTRYDATE) IN (3, 4, 5) THEN 'Spring' WHEN EXTRACT(MONTH FROM a.ENTRYDATE) IN (6, 7, 8) THEN 'Summer' ELSE 'Fall' END, b.avg_windspeed ORDER BY CASE WHEN season = 'Winter' THEN 1 WHEN season = 'Spring' THEN 2 WHEN season = 'Summer' THEN 3 WHEN season = 'Fall' THEN 4 END",
+        [cityName, city2Name]);
+        console.log(result.rows)
+        res.json(result.rows);
+        connection.close();
+    }
+    catch (error){
+        console.log(error);
+        res.status(400).json({message:error})
+    }
+})
+
+app.post('/examples6', async (req, res) => {
+    const cityName = req.body.cityName.cityName;
+    const city2Name = req.body.city2Name.city2Name;
+    console.log("City Name: " + cityName + " City Name: " + cityName);
+    try{
+        const connection = await oracledb.getConnection(config);
+        const ex5query = "SELECT EXTRACT(MONTH FROM a.ENTRYDATE) AS Month, AVG(a.AIRPRESSURE) AS City1, b.avg_airpressure AS City2 FROM ADAMELDREDGE.WEATHER a JOIN (SELECT EXTRACT(MONTH FROM ENTRYDATE) AS month, AVG(AirPressure) AS avg_airpressure FROM ADAMELDREDGE.WEATHER WHERE City = :cityName AND AirPressure IS NOT NULL AND EXTRACT(YEAR FROM ENTRYDATE) = '2021' GROUP BY EXTRACT(MONTH FROM ENTRYDATE)) b ON EXTRACT(MONTH FROM a.ENTRYDATE) = b.month WHERE a.AIRPRESSURE IS NOT NULL AND a.City = :city2Name AND EXTRACT(YEAR FROM a.ENTRYDATE) = '2021' GROUP BY EXTRACT(MONTH FROM a.ENTRYDATE), b.avg_airpressure ORDER BY 1";    //this should be a query (sql statement in a string)
+        const result = await connection.execute("SELECT EXTRACT(MONTH FROM a.ENTRYDATE) AS Month, AVG(a.AIRPRESSURE) AS City 1, b.avg_airpressure AS City 2 FROM ADAMELDREDGE.WEATHER a JOIN (SELECT EXTRACT(MONTH FROM ENTRYDATE) AS month, AVG(AirPressure) AS avg_airpressure FROM ADAMELDREDGE.WEATHER WHERE City = :cityName AND AirPressure IS NOT NULL AND EXTRACT(YEAR FROM ENTRYDATE) = '2021' GROUP BY EXTRACT(MONTH FROM ENTRYDATE)) b ON EXTRACT(MONTH FROM a.ENTRYDATE) = b.month WHERE a.AIRPRESSURE IS NOT NULL AND a.City = :city2Name AND EXTRACT(YEAR FROM a.ENTRYDATE) = '2021' GROUP BY EXTRACT(MONTH FROM a.ENTRYDATE), b.avg_airpressure ORDER BY 1",
+        [cityName, city2Name]);
         console.log(result.rows)
         res.json(result.rows);
         connection.close();
